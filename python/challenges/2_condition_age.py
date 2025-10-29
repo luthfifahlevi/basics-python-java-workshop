ba04a3278ea05f69ddbfa741bd558346ee8d7526
Neocode-1 <neocode.g1@gmail.com>
2025-10-29 11:08:38 +0700

    conditional

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,20 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
-
+name = input("Masukan nama Anda: ").strip()
+age = input("Masukan usia Anda: ").strip()
 # 2. TODO Tentukan kategori berdasarkan usia
+if age.isdigit():
+    age = int(age)
+    if age < 12:
+        category = "Anak-anak"
+    elif age < 18:
+        category = "Remaja"
+    elif age < 60:
+        category = "Dewasa"
+    else:
+        category = "Lansia"
+else:
+    category = "Invalid"
+# 3. TODO: Cetak kategori
+print(f"Halo {name}, Anda dikategorikan sebagai: {category}")
