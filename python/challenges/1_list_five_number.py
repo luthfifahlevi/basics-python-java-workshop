384c510969401e29df9214ea4126d43f74426a4d
Neocode-1 <neocode.g1@gmail.com>
2025-10-29 10:50:09 +0700

    Tambahkan editan

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -3,13 +3,14 @@ numbers = [10, 25, 7, 40, 15]
 print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
+print(f"Angka pertama: {numbers[0]}")
+print(f"Angka terakhir: {numbers[-1]}")
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
+numbers.append(30)
 print("List setelah ditambah:", numbers)
 
 # 4. TODO: Urutkan list secara descending
-
+# numbers.sort(reverse=True)
 # 5. Tampilkan hasil akhir
-print("List urut descending:", numbers)
+print("List terbalik:", numbers[::-1])
