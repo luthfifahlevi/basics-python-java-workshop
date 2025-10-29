8fa9b805f486b2939a5b8cb43fc9d25ae7b9b88a
muhammad.fahlevi <muhammad.fahlevi@gojek.com>
2025-10-29 11:07:38 +0700

    solve challenge 2 python

--- python/challenges/2_condition_age.py
+++ python/challenges/2_condition_age.py
@@ -1,5 +1,15 @@
 # Program Klasifikasi Usia
 
 # 1. TODO: Baca input dari pengguna
+usia = int(input("Masukkan usia Anda : "))
 
 # 2. TODO Tentukan kategori berdasarkan usia
+kat = "lansia"
+if usia < 12:
+    kat = "anak-anak"
+elif usia <= 17:
+    kat = "remaja"
+elif usia <= 59:
+    kat = "dewasa"
+
+print(kat)
